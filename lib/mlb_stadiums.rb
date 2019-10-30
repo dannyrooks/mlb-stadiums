0d97f3fbbfeb3302dfcba2bb9fd6caa93bbaf855
Daniel Rooker <dannyrooks@gmail.com>
2019-10-30 13:54:48 -0400

    first commit

---
+++ lib/mlb_stadiums.rb
@@ -0,0 +1,6 @@
+require "mlb_stadiums/version"
+
+module MlbStadiums
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mlb_stadiums/version.rb
@@ -0,0 +1,3 @@
+module MlbStadiums
+  VERSION = "0.1.0"
+end
