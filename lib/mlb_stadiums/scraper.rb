37388c480c2d66e1b38c5f4e973fd56518fc35a3
Daniel Rooker <dannyrooks@gmail.com>
2019-11-01 03:03:55 -0400

    finally got scrape_stadiums method working again, sorta

--- lib/mlb_stadiums/scraper.rb
+++ lib/mlb_stadiums/scraper.rb
@@ -3,15 +3,13 @@ class Scraper
     def self.scrape_stadiums
         html = open("https://www.ballparksofbaseball.com/national-league")
         doc = Nokogiri::HTML(html)
-        ballpark = Stadium.new #created a ballpark object
-        doc.css("a.stadium-item").each do |ballpark|
-            ballpark.title = ballpark.css("div.title").text.strip
-            binding.pry
+            stadium = Stadium.new
+        doc.css("a.stadium-item").each do |ballpark|	    
+            stadium.name = ballpark.css("div.title").text.strip	
+            
+            
+            
         end
-        # scrape the title and the link
-        #make movie objects and assign them titles and links
-
-
     end
 
     def self.scrape_stadium_details(stadium)
