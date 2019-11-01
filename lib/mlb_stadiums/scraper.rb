2363590f70ad4258d2464add2d80add8f0f20b2b
Daniel Rooker <dannyrooks@gmail.com>
2019-11-01 12:55:03 -0400

    |ballpark| is now |field|

--- lib/mlb_stadiums/scraper.rb
+++ lib/mlb_stadiums/scraper.rb
@@ -3,9 +3,9 @@ class Scraper
     def self.scrape_stadiums
         html = open("https://www.ballparksofbaseball.com/national-league")
         doc = Nokogiri::HTML(html)
-        doc.css("a.stadium-item").each do |ballpark|
+        doc.css("a.stadium-item").each do |field|
             stadium = Stadium.new
-            stadium.name = ballpark.css("div.title").text.strip	
+            stadium.name = field.css("div.title").text.strip	
             
         end
     end
