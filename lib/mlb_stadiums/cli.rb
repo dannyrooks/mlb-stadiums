af40685ff31297d50d9e687ce57c720a21fa3487
Daniel Rooker <dannyrooks@gmail.com>
2019-11-02 21:18:45 -0400

    only needs to grab specific facts from second scrape.

--- lib/mlb_stadiums/cli.rb
+++ lib/mlb_stadiums/cli.rb
@@ -2,24 +2,25 @@
 class CLI
 
     def run
-            puts ""
-            puts "Welcome to National League Baseball Stadium Fact Finder!"
-            puts ""
-            puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-            puts ""
-            puts "Choose the number of the stadium which you would like more information on, or type 'exit' to quit."
-            puts ""
-            puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-            puts ""
-
+        puts ""
+        puts "Welcome to National League Baseball Stadium Fact Finder!"
+        puts ""
+        puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        puts ""
+        puts "Choose the stadium which you would like to learn more about, or type 'exit' to quit."
+        puts ""
+        puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        puts ""
         Scraper.scrape_stadiums 
+        puts "NL Stadiums"
+        puts ""
         print_stadiums_with_index
-            puts " "
-            puts "Please enter a number:"
+        puts " "
+        puts "Please enter a number:"
         input = gets.strip.downcase
         while input != 'exit' do
-        stadium = Stadium.all[input.to_i - 1] #add validations
-        Scraper.scrape_stadium_details(stadium) if !stadium.facts 
+            stadium = Stadium.all[input.to_i - 1] #add validations
+            Scraper.scrape_stadium_details(stadium) if !stadium.facts 
             puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
             puts ""
             print_facts(stadium)
@@ -28,16 +29,17 @@ class CLI
             puts ""
             puts "Would you like to select another stadium?"
             puts "y or n?"
-        input = gets.strip.downcase 
-            if input == "y" 
+            input = gets.strip.downcase 
+        if input == "y" 
             print_stadiums_with_index
-                puts "Please enter a number:"
+            puts ""
+            puts "Please enter a number:"
             input = gets.strip.downcase
-            elsif input == "n"
-                goodbye
-            else
-                puts ""
-                puts "I do not understand, try again." 
+        elsif input == "n"
+            goodbye
+        else
+            puts ""
+            puts "I do not understand, try again." 
             input = gets.strip.downcase
         end
     end
